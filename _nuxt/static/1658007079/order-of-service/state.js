82843a0f481028b8ea8f61d1bb777971a3b2e2c8
rickscherer <rickscherer@users.noreply.github.com>
2022-07-16 21:32:03 +0000

    deploy: 7250ae9869bf24d6b475533d672932d2c03e94fd

--- _nuxt/static/1658007079/order-of-service/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658007079",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{name:"Constance Carmen Mercurio",dates:"December 31, 1926 - June 24, 2022"}}}("\u002Forder-of-service"));
