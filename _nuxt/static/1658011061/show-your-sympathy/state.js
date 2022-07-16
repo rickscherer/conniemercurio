74fbdebc4488418c6d5dc9252e4326b536e817be
rickscherer <rickscherer@users.noreply.github.com>
2022-07-16 22:37:42 +0000

    deploy: 2ee7dd68f7b5e69006d8e3b035182d4fbd83234b

---
+++ _nuxt/static/1658011061/show-your-sympathy/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1658011061",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{name:"Constance Carmen Mercurio",dates:"December 31, 1926 - June 24, 2022"}}}("\u002Fshow-your-sympathy"));
